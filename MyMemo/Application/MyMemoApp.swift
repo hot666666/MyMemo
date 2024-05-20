21051c8b8c93031b196bde9493479291ec21a034
hot666666 <hotcs6@naver.com>
2024-05-20 10:01:09 +0900

    chore : clean architecture

---
+++ MyMemo/Application/MyMemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyMemoApp.swift
+//  MyMemo
+//
+//  Created by 최하식 on 5/20/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMemo/Presentation/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MyMemo
+//
+//  Created by 최하식 on 5/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    ContentView()
+}
