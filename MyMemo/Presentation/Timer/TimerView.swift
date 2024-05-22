de441f39fb2ea455ea5f5da59782c30ef7a1ee02
hot666666 <hotcs6@naver.com>
2024-05-23 08:48:56 +0900

    feat : TimerView - TimerView, TimerViewModel

--- MyMemo/Presentation/Timer/TimerView.swift
+++ MyMemo/Presentation/Timer/TimerView.swift
@@ -8,11 +8,148 @@
 import SwiftUI
 
 struct TimerView: View {
+    @Environment(TimerViewModel.self) var timerViewModel
+    
     var body: some View {
-        Text("TimerView")
+        if !timerViewModel.isTimerView {
+            TimerSettingView()
+        } else {
+            TimerRunningView()
+        }
     }
 }
 
+struct TimerSettingView: View {
+    @Environment(TimerViewModel.self) var timerViewModel
+    
+    var body: some View {
+        VStack{
+            timerTitle
+            
+            VStack{
+                Spacer()
+                Divider()
+                TimePickerView()
+                Divider()
+                Spacer()
+            }
+            
+            Button(action: {
+                timerViewModel.setBtnTapped()
+            }, label: {
+                Text("설정하기")
+                    .bold()
+                    .foregroundColor(.primary)
+            })
+            .disabled(timerViewModel.isSettable)
+            
+            Spacer()
+        }
+    }
+    
+    var timerTitle: some View {
+        HStack{
+            Text("타이머")
+                .font(.title)
+                .bold()
+            Spacer()
+        }
+        .padding(.top, 20)
+        .padding(.leading, 20)
+    }
+}
+
+struct TimerRunningView: View {
+    @Environment(TimerViewModel.self) var timerViewModel
+    
+    var body: some View {
+        VStack{
+            TimerCircleView()
+            
+            HStack{
+                Button(action: {
+                    timerViewModel.cancelBtnTapped()
+                }, label: {
+                    CircleView(text: "취소", color: .black.opacity(0.5))
+                })
+                .disabled(!timerViewModel.isPaused)
+                .opacity(timerViewModel.isPaused ? 1 : 0.5)
+                
+                Spacer()
+                
+                Button(action: {
+                    timerViewModel.pauseOrRestartBtnTapped()
+                }, label: {
+                    CircleView(text: (timerViewModel.isPaused ? "시작" : "정지"), color: .orange.opacity(0.5))
+                })
+            }
+            .foregroundColor(.primary)
+            .padding(.horizontal, 20)
+        }
+        .onAppear{
+            timerViewModel.onAppearTimer()
+        }
+    }
+    
+    func CircleView(text: String, color: Color) -> some View {
+        Circle()
+            .fill(color)
+            .frame(width: 100, height: 100)
+            .overlay {
+                Text(text)
+            }
+    }
+}
+
+private struct TimerCircleView: View {
+    @Environment(TimerViewModel.self) var timerViewModel
+    
+    var body: some View {
+        ZStack{
+            Circle()
+                .trim(from: timerViewModel.animationValue, to: 1.0)
+                .stroke(Color.orange.opacity(0.9).gradient, lineWidth: 10)
+                .scaleEffect(x: 1, y: -1, anchor: .center)
+                .rotationEffect(.degrees(-90))
+            
+            Circle()
+                .stroke(Color.gray.opacity(0.2), lineWidth: 10)
+                .scaleEffect(x: 1, y: -1, anchor: .center)
+                .rotationEffect(.degrees(-90))
+                .overlay {
+                    Text(timerViewModel.timeRemaining.formattedLongTimeString)
+                        .font(.largeTitle)
+                        .monospaced()
+                }
+        }
+        .padding(20)
+    }
+}
+
+private struct TimePickerView: View {
+    @Environment(TimerViewModel.self) var timerViewModel
+    
+    var body: some View {
+        HStack(spacing: 0){
+            TimePickerComponent(selection: Bindable(timerViewModel).hour)
+            TimePickerComponent(selection: Bindable(timerViewModel).minute)
+            TimePickerComponent(selection: Bindable(timerViewModel).second)
+        }
+    }
+    
+    func TimePickerComponent(selection: Binding<Int>) -> some View {
+        Picker("Number", selection: selection) {
+            ForEach(0..<60) { number in
+                Text(String(format: "%02d", number)).tag(number)
+            }
+        }
+        .pickerStyle(.wheel)
+    }
+}
+
+
 #Preview {
-    TimerView()
+    let timerViewModel: TimerViewModel = .init()
+    return TimerView()
+        .environment(timerViewModel)
 }
---
+++ MyMemo/Presentation/Timer/TimerViewModel.swift
@@ -0,0 +1,93 @@
+//
+//  TimerViewModel.swift
+//  MyMemo
+//
+//  Created by 최하식 on 5/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class TimerViewModel {
+    var isTimerView: Bool = false
+    
+    var hour: Int = 0
+    var minute: Int = 0
+    var second: Int = 0
+    
+    var isPaused: Bool = true
+    
+    var timer: Timer?
+    
+    var timeRemaining: TimeInterval = 0
+    var timerTime: TimeInterval = 0
+    
+    var animationValue = 0.0
+    
+    var isSettable: Bool {
+        hour == 0 && minute == 0 && second == 0
+    }
+    
+    func setBtnTapped() {
+        timerTime = TimeInterval(3600*hour+60*minute+second)
+        timeRemaining = timerTime
+        isTimerView = true
+    }
+    
+    func pauseOrRestartBtnTapped() {
+        if isPaused {
+            startTimer()
+        } else {
+            timer?.invalidate()
+            timer = nil
+        }
+        isPaused.toggle()
+    }
+    
+    func cancelBtnTapped(){
+        stopTimer()
+        isTimerView = false
+    }
+    
+    func onAppearTimer() {
+        isPaused = true
+        animationValue = 0
+    }
+    
+    private func startTimer() {
+        guard timer == nil else { return }
+        
+        var backgroundTaskID: UIBackgroundTaskIdentifier?
+        backgroundTaskID = UIApplication.shared.beginBackgroundTask {
+            if let task = backgroundTaskID {
+                UIApplication.shared.endBackgroundTask(task)
+                backgroundTaskID = .invalid
+            }
+        }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+                withAnimation{
+                    self.animationValue = 1 - (Double(self.timeRemaining) / self.timerTime)
+                }
+            } else {
+                self.stopTimer()
+                //  self.notificationService.sendNotification()
+                
+                if let task = backgroundTaskID {
+                    UIApplication.shared.endBackgroundTask(task)
+                    backgroundTaskID = .invalid
+                }
+                
+                self.isTimerView = false
+            }
+        }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
