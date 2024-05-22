13545406430a13aafbd93532105f7e74cc66b837
hot666666 <hotcs6@naver.com>
2024-05-23 08:48:25 +0900

    chore : update TimeInterval extension

--- MyMemo/Common/Extension/TimeInterval+Extension.swift
+++ MyMemo/Common/Extension/TimeInterval+Extension.swift
@@ -13,4 +13,13 @@ extension TimeInterval {
         let seconds = Int(self) % 60
         return String(format: "%02d:%02d", minutes, seconds)
     }
+    
+    var formattedLongTimeString: String {
+        let totalSeconds = Int(self)
+        let hours = totalSeconds / 3600
+        let minutes = (totalSeconds % 3600) / 60
+        let seconds = totalSeconds % 60
+
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+    }
 }
