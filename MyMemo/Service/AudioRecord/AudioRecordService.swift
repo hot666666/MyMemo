2b0e465aefd872fa7e457042d9691009f0a6d6ff
hot666666 <hotcs6@naver.com>
2024-05-22 16:24:27 +0900

    feat : create AudioRecordService - start and stop recording with AVAudioRecorder

---
+++ MyMemo/Service/AudioRecord/AudioRecordService.swift
@@ -0,0 +1,58 @@
+//
+//  AudioRecordService.swift
+//  MyMemo
+//
+//  Created by 최하식 on 5/22/24.
+//
+
+import Foundation
+import AVFoundation
+
+enum AudioRecordServiceError: Error{
+    case initError
+}
+
+protocol AudioRecordServiceType {
+    func startRecording(fileURL: URL) async throws
+    func stopRecording() async -> URL? 
+}
+
+actor AudioRecordService: AudioRecordServiceType {
+    private var audioRecorder: AVAudioRecorder? = nil
+    private var settings: [String: Int] = [
+        AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+        AVSampleRateKey: 12000,
+        AVNumberOfChannelsKey: 1,
+        AVEncoderAudioQualityKey: AVAudioQuality.max.rawValue
+    ]
+    
+    private func initSession(){ /// AVAudioSession은 오디오 녹음 및 재생을 관리하는 싱글톤 객체, 녹음전 초기화 필요
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playAndRecord, mode: .default)
+            try session.setActive(true)
+        } catch {
+            print("Failed to set up audio session: \(error)")
+        }
+    }
+    
+    func startRecording(fileURL: URL) async throws {
+        do {
+            initSession()
+            audioRecorder = try AVAudioRecorder(url: fileURL, settings: settings)
+            audioRecorder?.record()
+        } catch {
+            throw AudioRecordServiceError.initError
+        }
+    }
+    
+    func stopRecording() async -> URL? {
+        if let audioRecorder = audioRecorder{
+            audioRecorder.stop()
+            self.audioRecorder = nil
+            return audioRecorder.url
+        }
+        return nil
+    }
+    
+}
